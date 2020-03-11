4cd5e399e76a6e3ee46cfa81a4c2a10458a63f42
planiflab <58193669+planiflab@users.noreply.github.com>
2020-03-11 10:34:51 -0300

    Add files via upload

---
+++ data/Polotics_3.js
@@ -0,0 +1,8 @@
+var json_Polotics_3 = {
+"type": "FeatureCollection",
+"name": "Polotics_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Nombre": "Parque TIC" }, "geometry": { "type": "Point", "coordinates": [ -68.819743335435803, -32.939030090988261 ] } }
+]
+}
